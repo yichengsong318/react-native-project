90f883e6dcd8abe66e383084e0d650b2dd01d9df
win <fancodeexpert@outlook.com>
2020-07-18 09:53:10 -0700

    Add Menu to GoalPartners

--- src/components/GoalPartners.js
+++ src/components/GoalPartners.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native';
+import { Menu, MenuTrigger, MenuOptions, MenuOption } from 'react-native-popup-menu';
 import UserAvatar from './UserAvatar';
+import * as appStyles from '../utils/styles';
 
 const GoalPartners = ({ goal }) => {
     return (
         <View style={styles.GoalPartners}>
             {goal.partners ?
                 goal.partners.map((partner) => (
-                    partner.user ?
-                        <UserAvatar
-                            key={partner.id}
-                            user={partner.user}
-                            style={styles.avatar}
-                        /> : null
+                    partner.user ? (
+                        <Menu>
+                            <MenuTrigger>
+                                <UserAvatar
+                                    key={partner.id}
+                                    user={partner.user}
+                                    style={styles.avatar}
+                                />
+                            </MenuTrigger>
+                            <MenuOptions>
+                                <MenuOption
+                                    customStyles={{
+                                        OptionTouchableComponent: TouchableWithoutFeedback
+                                    }}
+                                >
+                                    <View style={styles.menuHeader}>
+                                        <Text style={styles.partnerName}>{partner.user.firstName} {partner.user.lastName}</Text>
+                                        <Text>@{partner.user.username}</Text>
+                                    </View>
+                                </MenuOption>
+                                <MenuOption>
+                                    <Text style={styles.deleteLink}>Remove from Goal</Text>
+                                </MenuOption>
+                            </MenuOptions>
+                        </Menu>
+                    ) : null
                 )) : null
             }
         </View>
@@ -25,6 +47,18 @@ const styles = StyleSheet.create({
     },
     avatar: {
         marginLeft: 5,
+    },
+    partnerName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    menuHeader: {
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: appStyles.colors.divider,
+    },
+    deleteLink: {
+        color: appStyles.colors.danger,
     },
 });
 
